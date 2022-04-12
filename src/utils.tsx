d4675251035713b37b8cad726c4d4eb166a6c4bf
wonsss <bigdatajang@gmail.com>
2022-04-13 03:19:10 +0900

    :bug: 날짜 유틸 수정

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,8 +1,8 @@
 export const getDate = (timestamp: number) => {
   const date = new Date(timestamp);
   const convertedDate = `
-    ${date.getFullYear()}/${date.getMonth() + 1}/${
-    date.getDate() + 1
-  } ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    ${date.getFullYear()}/${
+    date.getMonth() + 1
+  }/${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
   return convertedDate;
 };
